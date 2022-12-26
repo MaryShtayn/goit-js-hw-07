66a68c5c9c052b0c0cfa881d18dd993084f6d9da
MaryShtayn <106402872+MaryShtayn@users.noreply.github.com>
2022-12-26 16:10:35 +0100

    finished task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryItem = document.querySelector('.gallery__item');
+const markup = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+      onclick="return false"
+    />
+  </a>`
+    ).join('');
+}
+gallery.insertAdjacentHTML("beforeend", markup);
+
+function onOpenModal(event) {
+    event.preventDefault();
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+    }
+  }
+  const lightbox = new SimpleLightbox('.gallery__item', { captionsData: 'alt', captionDelay: 250 });
+    lightbox.on('show.simplelightbox', function () {
+    gallery.addEventListener('click', onOpenModal);
+    });
+
+
